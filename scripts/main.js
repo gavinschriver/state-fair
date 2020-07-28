4da5e63bcf091a53f20a6bdf87ec1ea12f4a6d0f
Gavin Schriver <alexanderschriver@gmail.com>
2020-07-28 09:42:01 -0500

    initial commit

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+import { TicketBooth } from "./TicketBooth.js";
+import { RideTicketHolders } from "./rides/RideTicketHolders.js";
+
+// Import and invoke the ticket booth component function
+
+TicketBooth()
+RideTicketHolders()
