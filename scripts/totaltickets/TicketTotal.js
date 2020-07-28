5fba6eb0a1a2752930f4134ace15c250c1515bd7
Gavin Schriver <alexanderschriver@gmail.com>
2020-07-28 13:05:52 -0500

    EVEN CLOSER

--- scripts/totaltickets/TicketTotal.js
+++ scripts/totaltickets/TicketTotal.js
@@ -1,16 +1,16 @@
+
 const contentTarget = document.querySelector(".counter")
 const eventHub = document.querySelector("#state-fair")
 
 let someVar = 0
 
-contentTarget.innerHTML = `Total Tickets: `
+contentTarget.innerHTML = `Total Tickets: ${someVar}` 
 
 export const TicketTotal = () => {
     eventHub.addEventListener("gameTicketPurchased", customEvent => {
         someVar++
         
-        contentTarget.innerHTML = `
-        <div class="person rider">${someVar}</div>
+        contentTarget.innerHTML = `Total Tickets: ${someVar}
         `
     })
 }
